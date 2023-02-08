067971c8c63e8322cff3f642ec59975ce0187c58
Mohd Arbaz <53903338+MOHDARBAZ1@users.noreply.github.com>
2023-02-08 17:09:42 +0000

    comparison in datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,7 +26,55 @@ let valueInNUmber = Number(score)
 
 // String Conversion
 
-let aNumber = 44
-let aString = String(aNumber)
-console.log(aString)
-console.log(typeof aString)
+// let aNumber = 44
+// let aString = String(aNumber)
+// console.log(aString)
+// console.log(typeof aString)
+
+
+// Operations
+
+let value = 43
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = '   Arbaz'
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2;
+// console.table([num1, num2, num3])
+
+
+// postfix and prefix js mdn
+let gameCounter = 100;
+++gameCounter
+gameCounter++
+// console.log(gameCounter)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// strict check
+// console.log("2" == 2)
+// console.log("2" === 2)
