d3e03d465166960fe66dca02a68e0ed6827550fc
Mohd Arbaz <53903338+MOHDARBAZ1@users.noreply.github.com>
2023-02-08 16:38:01 +0000

    converion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+let valueInNUmber = Number(score)
+// console.log(typeof(valueInNUmber))
+// console.log(valueInNUmber)
+
+
+// Number conversion gives diffrent values
+// "12" => 12
+// "32abc" => Nan
+// null => 0
+// undefined => Nan
+// boolean =>0/1
+
+// Boolean Conversion
+// let isLoggedIn = 1
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
+// console.log(typeof booleanLoggedIn)
+// 1 => true, 0 => fasle
+// "" => false, "Arbaz" => true
+
+
+// String Conversion
+
+let aNumber = 44
+let aString = String(aNumber)
+console.log(aString)
+console.log(typeof aString)
